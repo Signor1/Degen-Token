f5d0e99e10b5bde776b1aec7ee9dd26bea51149d
signor1 <emmanuelomemgboji@gmail.com>
2024-02-24 02:24:16 +0100

    feat: deployed and verified

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,3 +14,7 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+//Degen ERC20 Token deployed to 0x1c615A625d3af403192dE4838eb5639B403Fc9D8
+
+// https://testnet.snowtrace.io/address/0x1c615A625d3af403192dE4838eb5639B403Fc9D8#code
