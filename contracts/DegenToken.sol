6fe6ca1441c113034ea050de0044a348a4c02abd
signor1 <emmanuelomemgboji@gmail.com>
2024-02-24 01:47:04 +0100

    feat: defined variables

---
+++ contracts/DegenToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract DegenToken {
+    //the state variables
+    string tokenName = "Degen";
+    string tokenSymbol = "DGN";
+    uint256 totalSupply;
+    address owner;
+}
