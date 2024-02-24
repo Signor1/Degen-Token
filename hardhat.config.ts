85e188f80550a7221eeba34b48c6a7f4e288d0c4
signor1 <emmanuelomemgboji@gmail.com>
2024-02-24 01:45:44 +0100

    feat: updated the hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,60 @@
-require("dotenv").config();
+require("dotenv").config({ path: ".env" });
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-//URL from alchemy
-const URL = process.env.URL;
+const ACCOUNT_PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-//private of the wallet
-const KEY = process.env.PRIVATE_KEY;
+const FORK_FUJI = false;
+const FORK_MAINNET = false;
+let forkingData = undefined;
+
+if (FORK_MAINNET) {
+  forkingData = {
+    url: "https://api.avax.network/ext/bc/C/rpcc",
+  };
+}
+if (FORK_FUJI) {
+  forkingData = {
+    url: "https://api.avax-test.network/ext/bc/C/rpc",
+  };
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
-    sepolia: {
-      url: URL,
-      accounts: [`0x${KEY}`],
+    hardhat: {
+      gasPrice: 225000000000,
+      chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
+      forking: forkingData,
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`],
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      fuji: "fuji", // apiKey is not required, just set a placeholder
+    },
+    customChains: [
+      {
+        network: "fuji",
+        chainId: 43113,
+        urls: {
+          apiURL:
+            "https://api.routescan.io/v2/network/testnet/evm/43113/etherscan",
+          browserURL: "https://testnet.snowtrace.io",
+        },
+      },
+    ],
   },
 };
 
